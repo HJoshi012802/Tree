fbb433849c0a96d41c909b129a4002a73e89d791
Harshit Joshi28 <harshitjoshi2002@gmail.com>
2023-06-07 10:33:36 +0530

    binary tree Depth First Traversal, Finding height ,Finding min

--- Tree/src/DF_traversal_binaryTree.java
+++ Tree/src/DF_traversal_binaryTree.java
@@ -1,5 +1,5 @@
 //PRE-ORDER TRAVERSAL Root->Left->Right
-public class Depth_first_traversal {
+public class DF_traversal_binaryTree {
     private static class Node{
         private final int value;
         private Node leftchild;
@@ -57,10 +57,28 @@ public class Depth_first_traversal {
         System.out.println(root.value);
         traversePreOrder(root.leftchild);
         traversePreOrder(root.rightchild);
+
+    }
+    public int height(){
+        return height(root);
+    }
+    private int height(Node root){
+        if(root==null) return -1;
+        if(root.leftchild==null && root.rightchild==null) return 0;
+        return 1+ Math.max(height(root.leftchild),height(root.rightchild));
+    }
+    public int minimum_value(){
+        return minimum_value(root);
+    }
+    private int minimum_value(Node root){
+        if(root.leftchild==null && root.rightchild==null) return root.value ;
+         var left =minimum_value(root.leftchild);
+         var right = minimum_value(root.rightchild);
+         return Math.min(Math.min(left,right),root.value);
     }
 
     public static void main(String[] args) {
-      Depth_first_traversal  tree=new Depth_first_traversal();
+      DF_traversal_binaryTree tree=new DF_traversal_binaryTree();
         tree.insert(7);
         tree.insert(4);
         tree.insert(9);
@@ -70,5 +88,7 @@ public class Depth_first_traversal {
         tree.insert(10);
         System.out.println(tree.find(5));
         tree.traversePreOrder();
+        System.out.println(tree.height());
+        System.out.println(tree.minimum_value());
     }
 }
