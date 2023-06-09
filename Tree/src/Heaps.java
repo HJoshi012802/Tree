d58e5e678b664d37cc3b67c88c78c2dca16cbfb7
Harshit Joshi28 <harshitjoshi2002@gmail.com>
2023-06-10 00:13:01 +0530

    commit Heap

---
+++ Tree/src/Heaps.java
@@ -0,0 +1,81 @@
+public class Heaps {
+
+    private int[]items=new int[10];
+    private int size;
+
+    public void insert(int value){
+        if(isFull()){
+            throw new IllegalStateException();
+        }
+        items[size++]=value;
+        bubbleUp();
+    }
+    public boolean isFull(){
+        return size ==items.length;
+    }
+
+    private void swap(int first,int second){
+        var temp=items[first];
+        items[first]=items[second];
+        items[second]=temp;
+    }
+    private void bubbleUp(){
+        var index =size-1;
+        while(index > 0 &&items[index]>items[parent(index)]){
+           swap(index,parent(index));
+           index=parent(index);
+        }}
+    private int parent(int index){return (index-1)/2;}
+    private int leftchildindex(int index){return index*2+1;}
+    private int rightchildindex(int index){return index*2+2;}
+
+
+        public void remove(){
+        if (isEmpty()){
+            throw new IllegalStateException();
+        }
+        items[0]=items[size-1];
+        size--;
+        int index =0;
+        while(index <=size&&!isValidParent(index)){
+        var largerChildIndex= largerChildindex(index);
+        swap(index,largerChildIndex);
+        index =largerChildIndex;
+        }
+
+    }
+    public boolean isEmpty(){
+       return size==0;
+    }
+    private int largerChildindex(int index){
+        return (leftchild(index)>rightchild(index)) ?
+                leftchildindex(index):rightchildindex(index);
+    }
+    private boolean isValidParent(int index){
+        return items[index] >= leftchild(index)&&
+                items[index] >= rightchild(index);
+    }
+    private int leftchild(int index){
+       return items[leftchildindex(index)];
+    }
+
+    private int rightchild(int index){
+        return items[rightchildindex(index)];
+    }
+
+    private void bubbleDown(){
+
+    }
+
+
+    public static void main(String[] args) {
+        Heaps heap=new Heaps();
+        heap.insert(10);
+        heap.insert(5);
+        heap.insert(17);
+        heap.insert(4);
+        heap.insert(22);
+        System.out.println("Done");
+    }
+
+}
